d8874ed02652643681197c7ea8a9cae40c2ed112
Mzcros <eleazerllance@gmail.com>
2025-06-12 13:25:06 -0500

    Update playlist.js

--- components/playlist.js
+++ components/playlist.js
@@ -1,1 +1,25 @@
+// components/playlist.js
 
+export const playlist = [
+  {
+    id: 1,
+    title: "Euphoria Dreams",
+    artist: "Nova",
+    src: "assets/audio/euphoria-dreams.mp3",
+    cover: "assets/images/euphoria.jpg"
+  },
+  {
+    id: 2,
+    title: "Velvet Sky",
+    artist: "Lumina",
+    src: "assets/audio/velvet-sky.mp3",
+    cover: "assets/images/velvet.jpg"
+  },
+  {
+    id: 3,
+    title: "Digital Pulse",
+    artist: "CyberWaves",
+    src: "assets/audio/digital-pulse.mp3",
+    cover: "assets/images/digital.jpg"
+  }
+];
